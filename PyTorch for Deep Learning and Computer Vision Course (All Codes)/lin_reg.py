601ab3c8aec4bd66e1159127eb0766465a5b4a27
Tom McCabe <tom.mccabe@missionsystems.com.au>
2022-02-13 16:44:46 +1100

    lin_red: Section 4 finished - Linear Reg

--- PyTorch for Deep Learning and Computer Vision Course (All Codes)/lin_reg.py
+++ PyTorch for Deep Learning and Computer Vision Course (All Codes)/lin_reg.py
@@ -1,19 +1,55 @@
 import torch
+from torch import optim
 from torch.autograd import grad
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 from torch.nn import Linear
 from icecream import ic
+from torch.nn.modules import module
 
-def tute1():
+# Define Linear Regression class
+# nn.Modulse is a base class
+class LR(nn.Module):
+    def __init__(self, input_size: int =1, output_size: int = 1) -> nn.Module:
+        super().__init__()
+        self.linear = nn.Linear(in_features=input_size, out_features=output_size)
+
+    def forward(self, x):
+        pred = self.linear(x)
+        return pred
+
+def tute17():
+    def get_params():
+        return w[0][0].item(), b[0].item()
+
+    def plot_fit(title):
+        w1, b1 = get_params()
+        x1 = np.array([-30, 30])
+        y1 = w1*x1+b1
+
+        plt.title(title)
+        plt.plot(X.numpy(), y.numpy(), 'o')
+        plt.plot(x1, y1, 'r')
+        plt.show()
+
+    # Make a noisy input datset
     X = torch.randn(100, 1)*10
     y = X + 3*torch.randn(100, 1)
-    plt.plot(X.numpy(), y.numpy(), 'o')
     plt.ylabel('y')
     plt.xlabel('x')
-    plt.show()
 
+    torch.manual_seed(1)
+    model = LR(1, 1)
+    ic(model)
+    [w, b] = model.parameters()
+    ic(w, b)
+
+    plot_fit('Initial Model')
+
+
+
+def tute14():
     def forward(x, w ,b):
         y = w*x + b
         return y
@@ -31,20 +67,41 @@ def tute1():
     x = torch.tensor([[2.0], [3.3]])
     ic(model(x))
 
-# Define Linear Regression class
-# nn.Modulse is a base class
-class LR(nn.Module):
-    def __init__(self, input_size: int =1, output_size: int = 1) -> nn.Module:
-        super().__init__()
-        self.linear = nn.Linear(in_features=input_size, out_features=output_size)
+def tute16():
+    model = LR(1, 1)
+    ic(list(model.parameters()))
 
-    def forward(self, x):
-        pred = self.linear(x)
-        return pred
+    x = torch.tensor([[1.0], [2.0]])
+    pred = model.forward(x)
+    ic(pred)
 
-model = LR(1, 1)
-ic(list(model.parameters()))
+# Loss Functions
+# distance error from y_hat to y
+# 
+def tute21():
+    torch.manual_seed(1)
+    X = torch.randn(100, 1)*10
+    y = X + 3*torch.randn(100, 1)
 
-x = torch.tensor([[1.0], [2.0]])
-pred = model.forward(x)
-ic(pred)
+    model = LR(1, 1)
+    criteon = nn.MSELoss()
+    optimiser = torch.optim.SGD(model.parameters(), lr=0.01) # stocastic gradient descent. Minimises loss per each sample
+
+    epochs = 100 # too few -> underfitting, too many -> overfitting
+    losses = []
+    for i in range(epochs):
+        y_pred = model.forward(X)
+        loss = criteon(y_pred, y)
+        print(f"epoch {i}, loss: {loss.item()}")
+
+        losses.append(loss)
+        optimiser.zero_grad()
+        loss.backward() # This needs to always be done before step
+        optimiser.step()
+
+    plt.plot(range(epochs), losses)
+    plt.ylabel('Losses')
+    plt.xlabel('Epochs')
+    plt.show()
+#run = tute17()
+run = tute21()
